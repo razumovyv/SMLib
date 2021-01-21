0dd1d2157eaa6b9935a3cb2fc2b80919a5cb3d87
Yuriy Razumov <razumov.uv@gmail.com>
2021-01-21 16:16:21 +0700

    Create DisplayImpl.h

---
+++ src/SMLib/Display/DisplayImpl.h
@@ -0,0 +1,10 @@
+//--------------------- File DisplayImpl.h -----------------------------
+#ifndef SML_DISPLAY_IMPLEMENTATION_H_
+#define SML_DISPLAY_IMPLEMENTATION_H_
+namspace sml{
+    namespace cls{
+
+    } // namespace cls
+} // namespace sml
+#endif
+//------------------ End of file DisplayImpl.h -------------------------
