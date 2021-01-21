cfae61aa6f0c2f3a0dcaeceff79b14d6dc8e84b3
Yuriy Razumov <razumov.uv@gmail.com>
2021-01-21 16:50:53 +0700

    Update Win32DisplayImpl.cpp

--- src/SMLib/Display/Win32/Win32DisplayImpl.cpp
+++ src/SMLib/Display/Win32/Win32DisplayImpl.cpp
@@ -1,3 +1,26 @@
 //---------------------- File Win32DisplayImpl.cpp----------------------------
 
+#include <windows.h>
+#include <vector>
+
+namespace sml{
+    namespace display{
+        class Display;
+    }
+    namespace cls{
+        std::vector<dispaly::Display> GetDisplayListFromSystem(){
+            
+            std::vector<display::Display> displayList;
+            DISPLAY_DEVICE displayInfo;
+            ZeroMemory(displayInfo&, sizeof(displayInfo));
+            
+            displayInfo.cb = sizeof(displayInfo);
+            
+            
+            return displayList;
+        }
+            
+    } // namespace cls
+} // namespace sml
+
 //------------------- End of file Win32DisplayImpl.cpp------------------------
