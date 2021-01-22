ca0842ef209f92f37dcecdddca13280f7a362c53
Yuriy Razumov <razumov.uv@gmail.com>
2021-01-22 13:24:15 +0700

    Update DisplayImpl.h

--- src/SMLib/Display/DisplayImpl.h
+++ src/SMLib/Display/DisplayImpl.h
@@ -3,7 +3,29 @@
 #define SML_DISPLAY_IMPLEMENTATION_H_
 namespace sml{
     namespace cls{
+            class DisplayImpl{
+                public:
+                    //--------------------------------------------------
+                    // Default constructor
+                    //--------------------------------------------------
+                    DisplayImpl();
+                    //--------------------------------------------------
+                    // Distructor
+                    //--------------------------------------------------
+                    virtual ~DisplayImpl();
 
+                    //--------------------------------------------------
+                    // Disable the cope constructor
+                    //--------------------------------------------------
+                    DisplayImpl(const Display&) = delete;
+
+                    //--------------------------------------------------
+                    // Disable th assigment operator
+                    //--------------------------------------------------
+                    DisplayImpl& operator=(const DisplayImpl&) = delete;
+                    
+
+            };
     } // namespace cls
 } // namespace sml
 #endif
