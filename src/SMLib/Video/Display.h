2fef3033a7b27772638bcd0c1c08d7c0653c6651
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-01 13:28:18 +0700

    Update Display.h

--- src/SMLib/Video/Display.h
+++ src/SMLib/Video/Display.h
@@ -13,69 +13,64 @@
 //#include <SMLib/Video/DisplayMode.h>
 
 
-namespace sml
-{
-    namespace video
-    {
-        class Display
-        {
-            public:
-                //-------------------------------------------------------------
-                // Destructor
-                //-------------------------------------------------------------
-                virtual ~Display();
+namespace sml { namespace video {
+    class Display {
+        public:
+            //-------------------------------------------------------------
+            // Destructor
+            //-------------------------------------------------------------
+            virtual ~Display();
 
-                friend class DisplayImpl;
+            friend class DisplayImpl;
             
             
-            public:
-                //-------------------------------------------------------------
-                // Get the supported modes list for current display
-                //-------------------------------------------------------------
-                std::vecotr<DisplayMode> GetDisplayModes() const;
+        public:
+            //-------------------------------------------------------------
+            // Get the supported modes list for current display
+            //-------------------------------------------------------------
+            std::vecotr<DisplayMode> GetDisplayModes() const;
 
-                //-------------------------------------------------------------
-                // Returning current mode of display
-                //-------------------------------------------------------------
-                DisplayMode GetCurrentMode() const;
+            //-------------------------------------------------------------
+            // Returning current mode of display
+            //-------------------------------------------------------------
+            DisplayMode GetCurrentMode() const;
 
-            protected:
+        protected:
 
-                //-------------------------------------------------------------
-                // Deafult constructor
-                //-------------------------------------------------------------
-                Display();
+            //-------------------------------------------------------------
+            // Deafult constructor
+            //-------------------------------------------------------------
+            Display();
 
-                //-------------------------------------------------------------
-                // Custom constructor
-                //-------------------------------------------------------------
-                Display(uint32_t index, 
-                        const char* diaplyName, 
-                        const char* displayDescription, 
-                        bool isPrimary);
+            //-------------------------------------------------------------
+            // Custom constructor
+            //-------------------------------------------------------------
+            Display(uint32_t index, 
+                    const char* diaplyName, 
+                    const char* displayDescription, 
+                    bool isPrimary);
                 
-                //-------------------------------------------------------------
-                // Disable a copy constructor
-                //-------------------------------------------------------------
-                Display(const Display&) = delete;
+            //-------------------------------------------------------------
+            // Disable a copy constructor
+            //-------------------------------------------------------------
+            Display(const Display&) = delete;
 
-                //-------------------------------------------------------------
-                // Disable a assigment operator
-                //-------------------------------------------------------------
-                Display& operator=(const Display&) = delete;
+            //-------------------------------------------------------------
+            // Disable a assigment operator
+            //-------------------------------------------------------------
+            Display& operator=(const Display&) = delete;
 
-            private:
+        private:
 
-                uint32_t        m_Index;        
-                std::string     m_Name;         
-                std::string     m_AdapterDescription;
-                std::string     m_MonitorDescription;
+            uint32_t        m_Index;        
+            std::string     m_Name;         
+            std::string     m_AdapterDescription;
+            std::string     m_MonitorDescription;
 
-                std::vector<DisplayMode> m_modesList;   
+            std::vector<DisplayMode> m_modesList;   
+        
+    }; // class SML_API Display
 
-        }; // class SML_API Display
-
-    } // namespace display
-} // namespace sml
+} /* namespace display */ } // namespace sml
 #endif // !SML_SYSTEM_DISPLAY_H_
 //-------------------------- End of file Dispaly.h ----------------------------
