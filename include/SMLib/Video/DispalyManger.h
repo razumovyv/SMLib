09edd77c2893743a9d91b187727752bcb0d79700
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-01 13:23:53 +0700

    Update DispalyManger.h

--- include/SMLib/Video/DispalyManger.h
+++ include/SMLib/Video/DispalyManger.h
@@ -9,65 +9,61 @@
 #include <SMLib/config.h>
 #include <map>
 
-namespace sml
-{  
-    namespace video
-    {
-        class SML_API DispalyManager{
+namespace sml{  namespace video {
+    class SML_API DispalyManager{
 
-            public:
-                //-------------------------------------------------------------
-                // Disable a copy constructor
-                //-------------------------------------------------------------
-                DipslayManager(const DispalyManager&) = delete;
+        public:
+            //-------------------------------------------------------------
+            // Disable a copy constructor
+            //-------------------------------------------------------------
+            DipslayManager(const DispalyManager&) = delete;
 
-                //-------------------------------------------------------------
-                // Disable a assigment operator
-                //-------------------------------------------------------------
-                DispalyManager operator=(const DispalyManager&) = delete;
+            //-------------------------------------------------------------
+            // Disable a assigment operator
+            //-------------------------------------------------------------
+            DispalyManager operator=(const DispalyManager&) = delete;
 
-                //-------------------------------------------------------------
-                // A fabric function
-                //-------------------------------------------------------------
-                static DisplayManager* GetInstance();
+            //-------------------------------------------------------------
+            // A fabric function
+            //-------------------------------------------------------------
+            static DisplayManager* GetInstance();
                 
-                //-------------------------------------------------------------
-                // Returning a number of an active displays in your system
-                //-------------------------------------------------------------
-                uint32_t GetNumberOfActiveDisplays() const;
+            //-------------------------------------------------------------
+            // Returning a number of an active displays in your system
+            //-------------------------------------------------------------
+            uint32_t GetNumberOfActiveDisplays() const;
                 
-                //-------------------------------------------------------------
-                // Returning a current video mode of primary display
-                //-------------------------------------------------------------
-                VideoMode GetPrimaryDisplayMode() const;
+            //-------------------------------------------------------------
+            // Returning a current video mode of primary display
+            //-------------------------------------------------------------
+            VideoMode GetPrimaryDisplayMode() const;
             
-            protected:
-                //-------------------------------------------------------------
-                // A Sinleton Instance
-                //-------------------------------------------------------------
-                static DisplayManager* _Instance;
+        protected:
+            //-------------------------------------------------------------
+            // A Sinleton Instance
+            //-------------------------------------------------------------
+            static DisplayManager* _Instance;
 
-                //-------------------------------------------------------------
-                // Default constructor
-                //-------------------------------------------------------------
-                DispalyManger();
+            //-------------------------------------------------------------
+            // Default constructor
+            //-------------------------------------------------------------
+            DispalyManger();
 
-                //-------------------------------------------------------------
-                // Default destructor
-                //-------------------------------------------------------------
-                ~DisplayManager();
+            //-------------------------------------------------------------
+            // Default destructor
+            //-------------------------------------------------------------
+            ~DisplayManager();
             
-            private:
-                //-------------------------------------------------------------
-                // The number of an active displays in yours operating system
-                //-------------------------------------------------------------
-                uint32_t m_NumOfDisplays;
-                std::map<std::string, sml::video::Dispaly> m_DisplayList;
+        private:
+            //-------------------------------------------------------------
+            // The number of an active displays in yours operating system
+            //-------------------------------------------------------------
+            uint32_t m_NumOfDisplays;
+            
+            std::map<std::string, sml::video::Dispaly> m_DisplayList;
                 
+    }; //  class DisplayManager
 
-        }; //  class DisplayManager
-
-    } // namespace video
-} // namspace sml
+} /* namespace video */ } // namspace sml
 #endif // !SML_VIDEO_DISPLAY_MANAGER_H_
 //----------------------- End of file DisplayManger.h -------------------------
