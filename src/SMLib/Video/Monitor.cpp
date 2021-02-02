c56c3ad1220f920154e8505d05255581d51e98d6
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-03 01:20:00 +0700

    Add a Monitor module
    A development of a module for working with monitors was started

---
+++ src/SMLib/Video/Monitor.cpp
@@ -0,0 +1,20 @@
+//-------------------------- File Monitor.cpp ---------------------------------
+
+#include <SMLib/Video/Monitor.h>
+
+namespace sml
+{
+    namespace video
+    {
+        Monitor::Monitor()
+        {
+
+        }
+
+        Monitor::~Monitor()
+        {
+            
+        }
+    }
+}
+//------------------------End of file Monitor.cpp -----------------------------
