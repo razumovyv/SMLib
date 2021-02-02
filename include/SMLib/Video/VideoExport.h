d26d4c5e2e7573d58c4b769f9c2c7114a41717f3
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-03 01:12:18 +0700

    Add file VideoExport.h
    In this file was defined the symbol for a import/export declaration.

---
+++ include/SMLib/Video/VideoExport.h
@@ -0,0 +1,16 @@
+//-------------------------- File VideoExport.h -------------------------------
+#ifndef SML_VIDEO_EXPORT_H_
+#define SML_VIDEO_EXPORT_H_
+//-----------------------------------------------------------------------------
+// Main configure file
+//-----------------------------------------------------------------------------
+#include <SMLib/config.h>
+
+#if defined(SML_VIDEO_EXPORT)
+    #define SML_VIDEO_API SML_API_EXPORT
+#else
+    #define SML_VIDEO_API SML_API_IMPORT
+#endif
+
+#endif // !SML_VIDEO_EXPORT_H_
+//----------------------- End of file VideoExport.h ---------------------------
