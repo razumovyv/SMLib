41adb6904d9147c941f389351000c0bb5769968f
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-03 06:55:52 +0700

    Add module with a new name

---
+++ src/SMLib/Video/Display.cpp
@@ -0,0 +1,20 @@
+//-------------------------- File Display.cpp ---------------------------------
+
+#include <SMLib/Video/Display.h>
+
+namespace sml
+{
+    namespace video
+    {
+        Display::Display()
+        {
+
+        }
+
+        Display::~Display()
+        {
+            
+        }
+    }
+}
+//------------------------End of file Display.cpp -----------------------------
