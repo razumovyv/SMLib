d73c8b0d8f0f4c4f619469d7af421c1a9ea66aaa
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-04 13:15:49 +0700

    Change Display class
    Add constructor with parameters

--- src/SMLib/Video/Display.cpp
+++ src/SMLib/Video/Display.cpp
@@ -6,7 +6,17 @@ namespace sml
 {
     namespace video
     {
-        Display::Display()
+        Display::Display(uint32_t index, 
+                         const char* adapterName, 
+                         const char* adapterDesc,
+                         const char* monitorDesc,
+                         const bool isPrimary
+                         const bool isActive) :
+                         m_Index(index),
+                         m_AdapterName(adapterName),
+                         m_AdapterDesc(adapterDesc),
+                         m_MonitorDesc(monitorDesc),
+                         m_isPrimary(isPrimary)  
         {
 
         }
