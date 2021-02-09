1a7bdb6696c2deb7848c6fd154f54fd470717521
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-09 20:58:17 +0700

    Update VideoMode.h and VideoMode.cpp
    1. Changed form a class to a struct
    2. Deleted destructor

--- include/SMLib/Video/VideoMode.h
+++ include/SMLib/Video/VideoMode.h
@@ -1,4 +1,4 @@
-//--------------------------- File VideoMode.h ------------------------------
+//--------------------------- File VideoMode.h -------------------------------
 #ifndef SML_VIDEO_MODE_H_
 #define SML_VIDEO_MODE_H_
 
@@ -12,51 +12,37 @@ namespace sml
 {
     namespace video
     {
-        class SML_VIDEO_API VideoMode
+        struct SML_VIDEO_API VideoMode
         {
-            public:
-            //-----------------------------------------------------------------
-            // Default constructor
-            // Initialize member dields to default values
-            //
-            // m_width           : 640
-            // m_height          : 480
-            // m_bitPerPixel     : 32
-            // m_DisplayFrecancy : 60 Hz
-            //-----------------------------------------------------------------
-            VideoMode();
+                //-----------------------------------------------------------------
+                // Default constructor
+                // Initialize member dields to default values
+                //
+                // m_width           : 640
+                // m_height          : 480
+                // m_bitPerPixel     : 32
+                // m_DisplayFrecancy : 60 Hz
+                //-----------------------------------------------------------------
+                VideoMode( );
 
-            //-----------------------------------------------------------------
-            // Custom constructor with parameters
-            //-----------------------------------------------------------------
-            VideoMode(uint32_t width 
-                    , uint32_t height 
-                    , uint32_t bitPerPixel = 32 
-                    , uint32_t frecency    = 60);
+                //-----------------------------------------------------------------
+                // Custom constructor with parameters
+                //-----------------------------------------------------------------
+                VideoMode( uint32_t width 
+                         , uint32_t height 
+                         , uint32_t bitPerPixel = 32 
+                         , uint32_t frecency    = 60 );
 
-            //-----------------------------------------------------------------
-            // Destructor
-            //-----------------------------------------------------------------
-            ~VideoMode();
-
-            //-----------------------------------------------------------------
-            // Returning "true" if the video mode was finded in a list 
-            // of supported modes.
-            //-----------------------------------------------------------------
-             bool isValid() const;
-
-            
-            private:
-            //-----------------------------------------------------------------
-            // Member data
-            //-----------------------------------------------------------------
-            uint32_t m_Width;           // Displays(windows) width
-            uint32_t m_Height;          // Displays(windows) heigth
-            uint32_t m_BitPerPixel;     // ColorDepth
-            uint32_t m_DisplayFrecency;
+                //-----------------------------------------------------------------
+                // Member data
+                //-----------------------------------------------------------------
+                uint32_t width;           
+                uint32_t height;          
+                uint32_t bitPerPixel;     
+                uint32_t displayFrecency;
 
         };
     } // namespace video
 } // namspace sml
 #endif // !SML_VIDEO_MODE_H_
-//------------------------ End of file VideoMode.h --------------------------
+//------------------------ End of file VideoMode.h ---------------------------
--- src/SMLib/Video/VideoMode.cpp
+++ src/SMLib/Video/VideoMode.cpp
@@ -13,10 +13,10 @@ namespace sml
         // Default constructor
         //---------------------------------------------------------------------
         VideoMode::VideoMode() 
-                    : m_Width(640) 
-                    , m_Height(480) 
-                    , m_BitPerPixel(32)
-                    , m_DisplayFrecency(60)
+                    : m_Width           ( 640 ) 
+                    , m_Height          ( 480 ) 
+                    , m_BitPerPixel     ( 32 )
+                    , m_DisplayFrecency ( 60 )
         {
 
         }
@@ -28,18 +28,10 @@ namespace sml
                             , uint32_t height 
                             , uint32_t bitPerPixel
                             , uint32_t frecancy) 
-                            : m_Width(width) 
-                            , m_Height(height) 
-                            , m_BitPerPixel(bitPerPixel)
-                            , m_DisplayFrecency(frecancy)
-        {
-
-        }
-
-        //---------------------------------------------------------------------
-        // Destructor
-        //---------------------------------------------------------------------
-        VideoMode::~VideoMode()
+                            : width           ( width ) 
+                            , height          ( height ) 
+                            , bitPerPixel     ( bitPerPixel )
+                            , displayFrecency ( frecancy )
         {
 
         }
