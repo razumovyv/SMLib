82565e84b8c62d31cf612df3abd38406e69e7828
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-10 23:55:27 +0700

    Added Type.h file to the video module

---
+++ include/SMLib/Video/Types.h
@@ -0,0 +1,28 @@
+//----------------------------- File Types.h ----------------------------------
+#ifndef SML_VIDEO_TYPES_H_
+#define SML_VIDEO_TYPES_H_
+
+#include <SMLib/Video/VideoExport.h>
+
+//-----------------------------------------------------------------------------
+// Flags for SetVideoMode
+//-----------------------------------------------------------------------------
+
+namespace sml{ namespace video{
+
+constexpr uint32_t SML_VM_REGUPDAETE  =  0x00000001;
+constexpr uint32_t SML_VM_TEST        =  0x00000002;
+constexpr uint32_t SML_VM_FULLSCREEN  =  0x00000004;
+constexpr uint32_t SML_VM_GLOBAL      =  0x00000008;
+constexpr uint32_t SML_VM_SET_PRIMARY =  0x00000010;
+constexpr uint32_t SML_VM_RESET       =  0x40000000;
+constexpr uint32_t SML_VM_NORESET     =  0x10000000;
+
+
+} // namespace video
+} // namespace sml
+
+
+
+#endif
+//-------------------------- End of file Types.h ------------------------------
