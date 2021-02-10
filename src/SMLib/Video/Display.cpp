d7d9e673023c1f43069961d560f1883db334b54c
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-10 23:57:27 +0700

    Update Display.h and Display.cpp
    DevCommit for syncronize.

--- src/SMLib/Video/Display.cpp
+++ src/SMLib/Video/Display.cpp
@@ -1,37 +1,84 @@
 //-------------------------- File Display.cpp ---------------------------------
 
 #include <SMLib/Video/Display.h>
+#include <SMLib/Video/DisplayImpl.h>
 
 namespace sml
 {
     namespace video
     {
+
+        //---------------------------------------------------------------------
+        // Default constructor
+        //---------------------------------------------------------------------
         Display::Display()
-            : m_Index(0)
-            , m_AdapterName("none")
-            , m_AdapterDesc("none")
-            , m_MonitorDesc("none")
-            , m_isPrimary(false) 
+            : index_       ( 0 )
+            , name_        ( "none" )
+            , adapterDesc_ ( "none" )
+            , monitorDesc_ ( "none" )
         { 
         }
 
-        Display::Display(uint32_t index, 
-                         const char* adapterName, 
-                         const char* adapterDesc,
-                         const char* monitorDesc,
-                         const bool isPrimary) 
-                        : m_Index(index)
-                        , m_AdapterName(adapterName)
-                        , m_AdapterDesc(adapterDesc)
-                        , m_MonitorDesc(monitorDesc)
-                        , m_isPrimary(isPrimary)  
+        //---------------------------------------------------------------------
+        // Base constructor
+        //---------------------------------------------------------------------
+        Display::Display( uint32_t index 
+                        , const char* displayName 
+                        , const char* adapterDesc
+                        , const char* monitorDesc) 
+                        : index_        ( index )
+                        , name_         ( displayName )
+                        , adapterDesc_  ( adapterDesc )
+                        , monitorDesc_  ( monitorDesc )
         {
 
         }
 
         Display::~Display()
         {
+
+        }
+
+        //---------------------------------------------------------------------
+        // Returning a current settings for instance
+        //---------------------------------------------------------------------
+        VideoMode Display::GetCurrentVideoMode() const
+        {
+            return cls::DisplayImpl::GetCurrentDesktopMode(this->adapterDesc_.c_str());
+        }
+
+        //---------------------------------------------------------------------
+        // Returning a vector of active displays
+        //---------------------------------------------------------------------
+        std::vector<Display> Display::GetActiveDesktopDispalys()
+        {
+            return cls::DisplayImpl::GetActiveDesktopDisplays();
+        }
+
+        //---------------------------------------------------------------------
+        // Get primary display instance
+        //---------------------------------------------------------------------
+        Display Display::GetPrimaryDisplay()
+        {
+            return cls::DisplayImpl::GetPrimaryDisplay();
+        }
+
+        //-------------------------------------------------------------
+        // Setting up a custom mode for this display
+        //-------------------------------------------------------------
+        bool Display::SetDesktopMode(const VideoMode& mode, uint32_t flags)
+        {
+            if(!IsValidMode(mode)) return false;
             
+            return true;
+        }
+
+        //-------------------------------------------------------------
+        // Checking a selected mode
+        //-------------------------------------------------------------
+        bool Display::IsValidMode(const VideoMode& mode) const
+        {
+            return cls::DisplayImpl::SetVideoMode(this->name_.c_str(), mode, SML_VM_TEST);
         }
     }
 }
