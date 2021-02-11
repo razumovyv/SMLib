075dda4464997a05f5a69a79fad88b2d402d59ba
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-11 22:05:19 +0700

    Update DisplayImpl.h and DisplayImpl.cpp
    Just a development commit

--- src/SMLib/Video/DisplayImpl.h
+++ src/SMLib/Video/DisplayImpl.h
@@ -4,31 +4,62 @@
 
 #include <vector>
 #include <SMLib/Video/Display.h>
-namespace sml{ namespace cls{
-    class DisplayImpl{
+#include <SMLib/Video/VideoMode.h>
+namespace sml { 
+    namespace cls {
+        class DisplayImpl{
 
-        public:
-            //-----------------------------------------------------------------
-            // Default constructor
-            //-----------------------------------------------------------------
-            DisplayImpl() {};
-            //-----------------------------------------------------------------
-            // Distructor
-            //-----------------------------------------------------------------
-            virtual ~DisplayImpl() {};
-                    
-             //----------------------------------------------------------------
-             // Returning a vector of active displays
-             //----------------------------------------------------------------
-            static std::vector<video::Display> GetDisplayListFromSystem();
+            public:
+                //-------------------------------------------------------------
+                // Default constructor
+                //-------------------------------------------------------------
+                DisplayImpl( ) {};
 
-            //-----------------------------------------------------------------
-            // Returning a vector of supported modes for display
-            //-----------------------------------------------------------------
-           // static std::vector<video::VideoMode> 
-           // GetDisplayModes(const std::string& displayName);
+                //-------------------------------------------------------------
+                // Destructor
+                //-------------------------------------------------------------
+                 ~DisplayImpl( ) {};
+
+                //-------------------------------------------------------------
+                // Get an active display by index
+                //-------------------------------------------------------------
+                static Display GetActiveDisplay(uint32_t index);    
+                
+                //-------------------------------------------------------------
+                // Returning a vector of active displays atached of desktops
+                //-------------------------------------------------------------
+                static std::vector<video::Display> 
+                GetActiveDesktopDisplays( );
+
+
+                
+                //-------------------------------------------------------------
+                // Get primary Display
+                //-------------------------------------------------------------
+                static Display GetPrimaryDisplay();
+
+                //-------------------------------------------------------------
+                // Returning a vector of supported modes for display
+                //-------------------------------------------------------------
+                 static std::vector<video::VideoMode> 
+                 GetDisplayModes(const char* displayName);
+
+                 //------------------------------------------------------------
+                 // Returning a current desktop mode for selected display
+                 //------------------------------------------------------------
+                 static video::VideoMode
+                 GetCurrentDesktopMode(const char* displayName);
+
+                 //------------------------------------------------------------
+                 // Seting a video mode to a selected display
+                 //------------------------------------------------------------
+                 static bool SetVideoMode(const char* displayName
+                                        , const VideoMode& mode
+                                        , uint32_t flags);
+
             
-    };
-} /* namespace cls*/ } // namespace sml
+        };
+    } // namespace cls
+} // namespace sml
 #endif
 //------------------ End of file DisplayImpl.h -------------------------
