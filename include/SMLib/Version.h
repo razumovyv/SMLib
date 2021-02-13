f1bb7589ea0959b4ef22c343453b1fdcff6e78ba
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-13 16:32:40 +0700

    Sinchronise with dev/modules/video

---
+++ include/SMLib/Version.h
@@ -0,0 +1,13 @@
+//-------------------------- FIle Version.h -----------------------------------
+#ifndef SML_VERSION_H_
+#define SML_VERSION_H_
+//-----------------------------------------------------------------------------
+// Don't edit! Genarated automaticaly
+//-----------------------------------------------------------------------------
+#define SML_VERSION_MJOR 1
+#define SML_VERSION_MINOR 0
+#define SML_VERSION_PATCH 0
+
+
+#endif // !SML_VERION_H_
+//---------------------- End of fIle Version.h --------------------------------
