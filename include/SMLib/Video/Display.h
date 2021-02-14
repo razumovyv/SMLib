1ba255f439c944c6b32cd53678301c9d1ab45ce8
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-14 18:26:43 +0700

    Update Display.h and Display.cpp
    Dev commit. Change Main Implementation

--- include/SMLib/Video/Display.h
+++ include/SMLib/Video/Display.h
@@ -5,7 +5,6 @@
 //-----------------------------------------------------------------------------
 // Headers
 //-----------------------------------------------------------------------------
-
 #include <vector>
 
 #include <SMLib/Video/Export.h>
@@ -13,71 +12,92 @@
 #include <SMLib/Video/Flags.h>
 
 
-namespace sml {
-
-    namespace video {
+namespace sml { namespace video {
         
-        class SML_VIDEO_API Display {
+    class SML_VIDEO_API Display {
 
-            public:
-                //-------------------------------------------------------------
-                // A default constructor
-                //-------------------------------------------------------------
-                Display( );
+        public:
+            //-------------------------------------------------------------
+            // A default constructor
+            //
+            // TODO: Documentations
+            //
+            //-------------------------------------------------------------
+            Display( );
 
-                Display(uint32_t index
-                      , const char* displayName
-                      , const char* adapterDesc
-                      , const char* monitorDesc
-                      , const bool isPrimary);
+            Display(  const char* displayName
+                    , const char* adapterDesc
+                    , const char* monitorDesc
+                    , const bool isPrimary);
 
-                //-------------------------------------------------------------
-                // A destructor
-                //-------------------------------------------------------------
-                ~Display( );
+            //-----------------------------------------------------------------
+            // A destructor
+            //
+            // TODO: Documentations
+            //
+            //-----------------------------------------------------------------
+            ~Display( );
 
-                //-------------------------------------------------------------
-                // Get a vector of active displayInstance with atached monitors
-                //-------------------------------------------------------------
-                static std::vector<Display> GetActiveDesktopDispalys();
+            //-----------------------------------------------------------------
+            // Returning a supported video modes list
+            //
+            // TODO: Documentations
+            //
+            //-----------------------------------------------------------------
+            std::vector< VideoMode > GetListVideoModes( ) const;
 
-                //-------------------------------------------------------------
-                // Get primary display instance
-                //-------------------------------------------------------------
-                static Display GetPrimaryDisplay();
+            //-----------------------------------------------------------------
+            // Doing request to the operating system
+            //
+            // TODO: Documentations
+            //
+            //-----------------------------------------------------------------
+            VideoMode GetVideoMode( ) const;
 
-                //-------------------------------------------------------------
-                // Doing request to the operating system
-                //-------------------------------------------------------------
-                VideoMode GetCurrentVideoMode( ) const;
+            //-----------------------------------------------------------------
+            // Setting up a custom mode for this display
+            //
+            // TODO: Documentations
+            //
+            //-----------------------------------------------------------------
+            bool SetVideoMode( const VideoMode& mode
+                             , uint32_t flags = SML_VM_FULLSCREEN );
 
-                //-------------------------------------------------------------
-                // Setting up a custom mode for this display
-                //-------------------------------------------------------------
-                bool SetDesktopMode(const VideoMode& mode
-                                        , uint32_t flags = SML_VM_FULLSCREEN);
+            //-----------------------------------------------------------------
+            // Checking a selected mode
+            //
+            // TODO: Documentations
+            //
+            //-----------------------------------------------------------------
+            bool IsValidMode( const VideoMode& mode ) const;
 
-                //-------------------------------------------------------------
-                // Checking a selected mode
-                //-------------------------------------------------------------
-                bool IsValidMode(const VideoMode& mode) const;
+            //-----------------------------------------------------------------
+            // Returning "true" if display is priamry
+            //
+            // TODO: Documentations
+            //
+            //-----------------------------------------------------------------
+            bool IsPrimary( ) const;
 
-                //-------------------------------------------------------------
-                // Returning "true" if display is priamry
-                //-------------------------------------------------------------
-                bool IsPrimary() const;
- 
-            private:
-                //-------------------------------------------------------------
-                // Member fields
-                //-------------------------------------------------------------
-                uint32_t    index_;        
-                string      name_;  
-                string      adapterDesc_;
-                string      monitorDesc_;
-                bool        isPrimary_;
-        };
-    } // namspace video
+            friend bool operator==( const Display& left, const Display& right );
+            friend bool operator!=( const Display& left, const Display& right );
+
+        private:
+            //-----------------------------------------------------------------
+            // Member fields
+            //
+            // TODO: Documentations
+            //
+            //-----------------------------------------------------------------
+
+            string      name_;  
+            string      adapterDesc_;
+            string      monitorDesc_;
+            bool        isPrimary_;
+            
+            std::vector< VideoMode> listModes_;
+    };
+} // namspace video
 } // namspace sml
 #endif // !SML_DISPLAY_H_
 //---------------------- End of file Display.h --------------------------------
--- src/SMLib/Video/Display.cpp
+++ src/SMLib/Video/Display.cpp
@@ -1,84 +1,121 @@
 //-------------------------- File Display.cpp ---------------------------------
 
 #include <SMLib/Video/Display.h>
-#include <SMLib/Video/DisplayImpl.h>
+#include "DisplayImpl.h"
 
-namespace sml {
+namespace sml {  namespace video {
 
-    namespace video {
+    //---------------------------------------------------------------------
+    // Default constructor
+    //
+    // TODO: Documentations
+    //
+    //---------------------------------------------------------------------
+    Display::Display(){ 
 
-        //---------------------------------------------------------------------
-        // Default constructor
-        //---------------------------------------------------------------------
-        Display::Display()
-            : index_        ( 0 )
-            , name_         ( "none" )
-            , adapterDesc_  ( "none" )
-            , monitorDesc_  ( "none" )
-            , isPrimary_    ( false ) { }
-
-        //---------------------------------------------------------------------
-        // Base constructor
-        //---------------------------------------------------------------------
-        Display::Display( uint32_t index 
-                        , const char* displayName 
-                        , const char* adapterDesc
-                        , const char* monitorDesc
-                        , const bool isPrimary) 
-                        : index_        ( index )
-                        , name_         ( displayName )
-                        , adapterDesc_  ( adapterDesc )
-                        , monitorDesc_  ( monitorDesc )
-                        , isPrimary_    ( isPrimary ) { }
-
-        Display::~Display() { }
-
-        //---------------------------------------------------------------------
-        // Returning a current settings for instance
-        //---------------------------------------------------------------------
-        VideoMode Display::GetCurrentVideoMode() const {
-
-            return cls::DisplayImpl::GetCurrentDesktopMode(this->adapterDesc_.c_str());
-        }
-
-        //---------------------------------------------------------------------
-        // Returning a vector of active displays
-        //---------------------------------------------------------------------
-        std::vector<Display> Display::GetActiveDesktopDispalys() {
-
-            return cls::DisplayImpl::GetActiveDesktopDisplays();
-        }
-
-        //---------------------------------------------------------------------
-        // Get primary display instance
-        //---------------------------------------------------------------------
-        Display Display::GetPrimaryDisplay(){
-
-            return cls::DisplayImpl::GetPrimaryDisplay();
-        }
-
-        //-------------------------------------------------------------
-        // Setting up a custom mode for this display
-        //-------------------------------------------------------------
-        bool Display::SetDesktopMode(const VideoMode& mode, uint32_t flags) {
-
-            if(!IsValidMode(mode)) return false;
-            
-            return cls::DisplayImpl::SetVideoMode(this->name_.c_str(), mode, flags);
-        }
-
-        //-------------------------------------------------------------
-        // Checking a selected mode
-        //-------------------------------------------------------------
-        bool Display::IsValidMode(const VideoMode& mode) const {
-
-            return cls::DisplayImpl::SetVideoMode(this->name_.c_str(), mode, SML_VM_TEST);
-        }
-
-        bool Display::IsPrimary() const {
-            
-            return isPrimary_;
-        }
+        *(this) = cls::DisplayImpl::GetPrimaryDisplay();
+        listModes_ = cls::DisplayImpl::GetListVideoModes(name_.c_str());
+   
     }
-}
+
+    //---------------------------------------------------------------------
+    // Base constructor
+    //
+    // TODO: Documentations
+    //
+    //---------------------------------------------------------------------
+    Display::Display( const char* displayName 
+                    , const char* adapterDesc
+                    , const char* monitorDesc
+                    , const bool isPrimary) 
+                    : name_         ( displayName )
+                    , adapterDesc_  ( adapterDesc )
+                    , monitorDesc_  ( monitorDesc )
+                    , isPrimary_    ( isPrimary ) 
+    { 
+        listModes_ = cls::DisplayImpl::GetListVideoModes(name_.c_str());
+    }
+
+    Display::~Display() {listModes_.clear(); }
+
+    //---------------------------------------------------------------------
+    // Returning a vector of active displays
+    //
+    // TODO: Documentations
+    //
+    //---------------------------------------------------------------------
+    std::vector<Display> Display::GetActiveDesktopDispalys() {
+
+        return cls::DisplayImpl::GetActiveDesktopDisplays();
+    }
+
+    //-----------------------------------------------------------------
+    // Returning a supported video modes list
+    //
+    // TODO: Documentations
+    //
+    //-----------------------------------------------------------------
+    std::vector< VideoMode> Display::GetListVideoModes( ) const {
+
+        return listModes_;
+    }   
+
+    //---------------------------------------------------------------------
+    // Returning a current settings for instance
+    //
+    // TODO: Documentations
+    //
+    //---------------------------------------------------------------------
+    VideoMode Display::GetVideoMode( ) const {
+
+        return cls::DisplayImpl::GetVideoMode( name_.c_str() );
+    }
+
+    //-------------------------------------------------------------
+    // Setting up a custom mode for this display
+    //
+    // TODO: Documentations
+    //
+    //-------------------------------------------------------------
+    bool Display::SetVideoMode( const VideoMode& mode, uint32_t flags ) {
+
+        if( !IsValidMode(mode) ) return false;
+        
+        return cls::DisplayImpl::SetVideoMode( name_.c_str(), mode, flags );
+    }
+
+    //-------------------------------------------------------------
+    // Checking a selected mode
+    //
+    // TODO: Documentations
+    //
+    //-------------------------------------------------------------
+    bool Display::IsValidMode( const VideoMode& mode ) const {
+
+        return cls::DisplayImpl::SetVideoMode( name_.c_str(), mode, SML_VM_TEST );
+    }
+
+    // TODO: Description
+    // TODO: Documentation
+    bool Display::IsPrimary() const {
+        
+        return isPrimary_;
+    }
+
+    bool operator==( const Display& left, const Display& right ) {
+        
+        return (( left.name_        == right.name_        )
+              &&( left.adapterDesc_ == right.adapterDesc_ )
+              &&( left.monitorDesc_ == right.monitorDesc_ )
+              &&( left.isPrimary_   == right.isPrimary_   ));
+    }
+    bool operator!=( const Display& left, const Display& right ) {
+    
+        return !( ( left.name_        == right.name_        )
+                &&( left.adapterDesc_ == right.adapterDesc_ )
+                &&( left.monitorDesc_ == right.monitorDesc_ )
+                &&( left.isPrimary_   == right.isPrimary_   ));
+    }
+} // namespace video
+} // namespace sml
 //------------------------End of file Display.cpp -----------------------------
