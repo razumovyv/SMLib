82fce2e808c2a82d2e3b98f1224081e4b3776865
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-14 18:22:46 +0700

    Update Flags.h

--- include/SMLib/Video/Flags.h
+++ include/SMLib/Video/Flags.h
@@ -2,12 +2,11 @@
 #ifndef SML_VIDEO_TYPES_H_
 #define SML_VIDEO_TYPES_H_
 
+namespace sml{ namespace video{
+
 //-----------------------------------------------------------------------------
 // Flags for SetVideoMode
 //-----------------------------------------------------------------------------
-
-namespace sml{ namespace video{
-
 constexpr uint32_t SML_VM_REGUPDAETE  =  0x00000001;
 constexpr uint32_t SML_VM_TEST        =  0x00000002;
 constexpr uint32_t SML_VM_FULLSCREEN  =  0x00000004;
@@ -15,7 +14,6 @@ constexpr uint32_t SML_VM_GLOBAL      =  0x00000008;
 constexpr uint32_t SML_VM_SET_PRIMARY =  0x00000010;
 constexpr uint32_t SML_VM_RESET       =  0x40000000;
 constexpr uint32_t SML_VM_NORESET     =  0x10000000;
-
 
 } // namespace video
 } // namespace sml
