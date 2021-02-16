e767312d6a5d8d751161f5b1aa84883684913ffa
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-16 22:43:16 +0700

    Update config.cmake

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,11 +1,15 @@
+###############################################################################
+# Main cmake config file
+###############################################################################
+
+set (CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
+set (CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
+# Set the prefix for install directory
+set(CMAKE_INSTALL_PREFIX ${PROJECT_SOURCE_DIR}/out/install/${PROJECT_NAME})
+
 # Detect th operating system
 if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
     set(SML_WINDOWS_SYSTEM 1)
 else()
     return()
 endif()
-
-set (CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
-set (CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
-# Set the prefix for install directory
-set(CMAKE_INSTALL_PREFIX ${PROJECT_SOURCE_DIR}/out/install/${PROJECT_NAME})
