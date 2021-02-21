9ad82efbada3e8bc6d9cd83c222f653e85765c79
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-21 10:50:01 +0700

    Update Config.h
    A platform detection was moved to Config.h
    File PlatformDetection.h was deleted.

--- include/SMLib/config.h
+++ include/SMLib/config.h
@@ -16,10 +16,63 @@
 //-----------------------------------------------------------------------------
 // Platform detection
 //-----------------------------------------------------------------------------
-#include <SMLib/PlatformDetection.h>
+#if defined(_WIN32) || defined(_WIN32_)
+
+    #define SML_WINDOWS_SYSTEM
+    #ifndef NOMINMAX
+        #define NOMINMAX
+    #endif
+
+#elif defined(__APPLE__) && defined(__MACH__)
+    
+    #define SML_APPLE_SYSTEM
+    #error Apple operating systems don't supported by Simple Game Engine
+
+#elif defined(__unix__)
+    
+    // Indentify Unix operaring systems
+
+    #if defined(__linux__)
+
+        // Linux
+        #define SML_LINUX_SYSTEM
+
+    #elif defined(__ANDROID__)
+        // Android
+        #define SML_ANDROID_SYSTEM
+        #error Android operating system don't supported by the Simple Game Engine
+    
+    #elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+
+        // FreeBSD
+        #define SML_FREEBSD_SYSTEM
+        #error FreeBSD operating system don't supported the Simple Game Engine
+
+    #elif defined(__OpenBSD__)
+
+        // OpenBSD
+        #define SML_OPENBSD_SYSTEM
+        #error OpenBSD operating system don't supported the Simple Game Engine
+
+    #elif defined(__NetBSD__)
+
+        // NetBSD
+        #define SML_NETBSD_SYSTEM
+        #error NetBSD operating system don't supported by Simple Game Engine
+
+    #else
+
+        // Unsupported UNIX system
+        #error This UNIX operating system is not supported by Simple Game Engine
+
+    #endif
+#else
+    #error This system do not support by the Simple Game Emgine
+#endif
 
 //-----------------------------------------------------------------------------
 // Config API import/export
+//-----------------------------------------------------------------------------
 #if !defined(SML_STATIC)
     #if defined(SML_WINDOWS_SYSTEM)
 
@@ -112,7 +165,6 @@ namespace sml{
 // Using types
 //-----------------------------------------------------------------------------
 #include <cstdint>
-#include <string>
 
 namespace sml
 {
