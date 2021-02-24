c070e8dc42a88a2c6343f06ee5ddc3989763ad4b
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-24 10:50:08 +0700

    Update VideoMode.h
    Changed code style

--- include/SMLib/Video/VideoMode.h
+++ include/SMLib/Video/VideoMode.h
@@ -11,36 +11,36 @@ namespace sml {
 
 namespace video {
 
-    struct SML_VIDEO_API VideoMode {
+struct SML_VIDEO_API VideoMode {
 
-            //-----------------------------------------------------------------
-            // Default constructor
-            // Initialize member dields to default values
-            //
-            // m_width           : 640
-            // m_height          : 480
-            // m_bitPerPixel     : 32
-            // m_DisplayFrecancy : 60 Hz
-            //-----------------------------------------------------------------
-        VideoMode( );
+        //-----------------------------------------------------------------
+        // Default constructor
+        // Initialize member dields to default values
+        //
+        // m_width           : 640
+        // m_height          : 480
+        // m_bitPerPixel     : 32
+        // m_DisplayFrecancy : 60 Hz
+        //-----------------------------------------------------------------
+    VideoMode( );
 
-            //-----------------------------------------------------------------
-            // Custom constructor with parameters
-            //-----------------------------------------------------------------
-        VideoMode( uint32_t width,
-                    uint32_t height, 
-                    uint32_t bitPerPixel = 32, 
-                    uint32_t frecency    = 60 );
-                
-            //-----------------------------------------------------------------
-            // Member data
-            //-----------------------------------------------------------------
-            uint32_t width;           
-            uint32_t height;          
-            uint32_t bitPerPixel;     
-            uint32_t displayFrecency;
+        //-----------------------------------------------------------------
+        // Custom constructor with parameters
+        //-----------------------------------------------------------------
+    VideoMode( uint32_t width,
+                uint32_t height, 
+                uint32_t bitPerPixel = 32, 
+                uint32_t frecency    = 60 );
 
-    }; // struct VideoMode
+        //-----------------------------------------------------------------
+        // Member data
+        //-----------------------------------------------------------------
+        uint32_t width;           
+        uint32_t height;          
+        uint32_t bitPerPixel;     
+        uint32_t displayFrecency;
+
+}; // struct VideoMode
 
 //-----------------------------------------------------------------------------
 // TODO Documentation
