945ba7a64e4cf89c063a916096a17ae9feb67cb5
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-24 10:58:22 +0700

    Update VideoMode.cpp
    Changed a code style

--- src/SMLib/Video/VideoMode.cpp
+++ src/SMLib/Video/VideoMode.cpp
@@ -7,60 +7,57 @@
 #include <SMLib/Video/VideoMode.h>
 
 namespace sml {
+namespace video {
 
-    namespace video {
+//---------------------------------------------------------------------
+// Default constructor
+//---------------------------------------------------------------------
+VideoMode::VideoMode() 
+: width           (640),
+  height          (480), 
+  bitPerPixel     (32),
+  displayFrecency (60) { }
 
-        //---------------------------------------------------------------------
-        // Default constructor
-        //---------------------------------------------------------------------
-        VideoMode::VideoMode() 
-        : width           ( 640 ),
-          height          ( 480 ), 
-          bitPerPixel     ( 32 ),
-          displayFrecency ( 60 ) { }
+//---------------------------------------------------------------------
+// Custom constructor
+//---------------------------------------------------------------------
+VideoMode::VideoMode(uint32_t width, uint32_t height, 
+                     uint32_t bitPerPixel, uint32_t frecancy) 
+: width           ( width ), 
+  height          ( height ), 
+  bitPerPixel     ( bitPerPixel ),
+  displayFrecency ( frecancy ) { }
 
-        //---------------------------------------------------------------------
-        // Custom constructor
-        //---------------------------------------------------------------------
-        VideoMode::VideoMode( uint32_t width, 
-                              uint32_t height, 
-                              uint32_t bitPerPixel,
-                              uint32_t frecancy) 
-        : width           ( width ), 
-          height          ( height ), 
-          bitPerPixel     ( bitPerPixel ),
-          displayFrecency ( frecancy ) { }
+bool operator ==(const VideoMode& left, const VideoMode& right)
+{
+     auto _left = std::tie( left.width, 
+                            left.height, 
+                            left.bitPerPixel,
+                            left.displayFrecency );
 
-        bool operator ==(const VideoMode& left, const VideoMode& right)
-            {
-                 auto _left = std::tie( left.width, 
-                                        left.height, 
-                                        left.bitPerPixel,
-                                        left.displayFrecency );
+    auto  _right = std::tie( right.width,
+                             right.height,
+                             right.bitPerPixel,
+                             right.displayFrecency );
 
-                auto  _right = std::tie( right.width,
-                                         right.height,
-                                         right.bitPerPixel,
-                                         right.displayFrecency );
+    return (_left == _right);
+}
 
-                return (_left == _right);
-            }
+bool operator !=(const VideoMode& left, const VideoMode& right)
+{
+    auto _left = std::tie( left.width, 
+                           left.height, 
+                           left.bitPerPixel,
+                           left.displayFrecency );
 
-        bool operator !=(const VideoMode& left, const VideoMode& right)
-            {
-                auto _left = std::tie( left.width, 
-                                       left.height, 
-                                       left.bitPerPixel,
-                                       left.displayFrecency );
+    auto  _right = std::tie( right.width,
+                             right.height,
+                             right.bitPerPixel,
+                             right.displayFrecency );
 
-                auto  _right = std::tie( right.width,
-                                         right.height,
-                                         right.bitPerPixel,
-                                         right.displayFrecency );
+    return (_left != _right);
+}
 
-                return (_left != _right);
-            }
-
-    } // namespace video
+} // namespace video
 } // namspace sml
 //--------------- End of file DisplayMode.cpp ---------------------------------
