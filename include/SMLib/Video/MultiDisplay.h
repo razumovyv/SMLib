4b91dfcfbf759c181ee779d74ad9ccaecd51456d
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-24 10:53:19 +0700

    Update MultiDisplay.h
    Change code style

--- include/SMLib/Video/MultiDisplay.h
+++ include/SMLib/Video/MultiDisplay.h
@@ -11,60 +11,60 @@
 namespace sml { 
 namespace video {
 
-	class SML_VIDEO_API MultiDisplay : system::NoneCopyable {
+class SML_VIDEO_API MultiDisplay : system::NoneCopyable {
 
-		public:
+	public:
 
-			//-----------------------------------------------------------------
-			// Destructor
-			//-----------------------------------------------------------------
-			~MultiDisplay( );
+		//-----------------------------------------------------------------
+		// Destructor
+		//-----------------------------------------------------------------
+		~MultiDisplay( );
 
-			//-----------------------------------------------------------------
-			// Factory function for returning singleton
-			//-----------------------------------------------------------------
-			static MultiDisplay* 
-			GetMultiDisplays( const bool useMultidisplay = SML_MD_PRIMARY_ONLY );
+		//-----------------------------------------------------------------
+		// Factory function for returning singleton
+		//-----------------------------------------------------------------
+		static MultiDisplay* 
+		GetMultiDisplays( const bool useMultidisplay = SML_MD_PRIMARY_ONLY );
 
-			//-----------------------------------------------------------------
-			// Returnin nuber of display instances
-			//-----------------------------------------------------------------
-			size_t GetNumberDisplays() const;
+		//-----------------------------------------------------------------
+		// Returnin nuber of display instances
+		//-----------------------------------------------------------------
+		size_t GetNumberDisplays() const;
 
-			//-----------------------------------------------------------------
-			// Returning a reference to a display instance
-			//
-			// FIXME
-			//-----------------------------------------------------------------
-			Display GetDisplayRef( const uint32_t& index ) const;
+		//-----------------------------------------------------------------
+		// Returning a reference to a display instance
+		//
+		// FIXME
+		//-----------------------------------------------------------------
+		Display GetDisplayRef( const uint32_t& index ) const;
 
 
 
-		private:
-			
-			//-----------------------------------------------------------------
-			// Default constructor
-			//-----------------------------------------------------------------
-			MultiDisplay( );
-			
+	private:
 
-			//-----------------------------------------------------------------
-			// Conteiner for diaplys instances
-			// FIXME displays container
-			//-----------------------------------------------------------------
-			std::vector< Display > displays_;
+		//-----------------------------------------------------------------
+		// Default constructor
+		//-----------------------------------------------------------------
+		MultiDisplay( );
 
-			//-----------------------------------------------------------------
-			// Adding a display instance to a container
-			//-----------------------------------------------------------------
-			void RegInstance( const Display &regInst );
 
-			//-----------------------------------------------------------------
-			// Clear container with displays
-			//-----------------------------------------------------------------
-			void ClearDisplayInstances();
+		//-----------------------------------------------------------------
+		// Conteiner for diaplys instances
+		// FIXME displays container
+		//-----------------------------------------------------------------
+		std::vector< Display > displays_;
 
-	}; // class DisplayAccess
+		//-----------------------------------------------------------------
+		// Adding a display instance to a container
+		//-----------------------------------------------------------------
+		void RegInstance( const Display &regInst );
+
+		//-----------------------------------------------------------------
+		// Clear container with displays
+		//-----------------------------------------------------------------
+		void ClearDisplayInstances();
+
+}; // class DisplayAccess
 } // namespace video
 } // namespace sml
 #endif // !SML_MULTI_DISPLAYS_H_
