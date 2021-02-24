2306db76f125c9fc01e2527c13eccce659b33d01
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-24 11:03:36 +0700

    Update Display.cpp
    Change a code style

--- src/SMLib/Video/Display.cpp
+++ src/SMLib/Video/Display.cpp
@@ -3,143 +3,142 @@
 #include <SMLib/Video/Display.h>
 #include "DisplayImpl.h"
 
-namespace sml {  namespace video {
+namespace sml {  
+namespace video {
 
-    //---------------------------------------------------------------------
-    // Default constructor
-    //
-    // TODO: Documentations
-    //
-    //---------------------------------------------------------------------
-    Display::Display() : 
-    name_               ("none"),
-    adapterDescription_ ("none"),
-    monitorDescription_ ("none"),
-    isPrimary_          (false) { }
+//---------------------------------------------------------------------
+// Default constructor
+//
+// TODO: Documentations
+//
+//---------------------------------------------------------------------
+Display::Display() 
+    : name_               ("none"),
+      adapterDescription_ ("none"),
+      monitorDescription_ ("none"),
+      isPrimary_          (false) { }
 
-    //---------------------------------------------------------------------
-    // Base constructor
-    //
-    // TODO: Documentations
-    //
-    //---------------------------------------------------------------------
-    Display::Display( const std::string &name, 
-                      const std::string &adapterDescription, 
-                      const std::string &monitorDescription, 
-                      const bool        isPrimary ) : 
-                      name_               ( name ), 
-                      adapterDescription_ ( adapterDescription ), 
-                      monitorDescription_ ( monitorDescription ), 
-                      isPrimary_          ( isPrimary   ) { }
+//---------------------------------------------------------------------
+// Base constructor
+//
+// TODO: Documentations
+//
+//---------------------------------------------------------------------
+Display::Display(const std::string &name, 
+                 const std::string &adapterDescription, 
+                 const std::string &monitorDescription, 
+                 const bool        isPrimary) 
+    : name_               (name), 
+      adapterDescription_ (adapterDescription), 
+      monitorDescription_ (monitorDescription), 
+      isPrimary_          (isPrimary) { }
 
-    Display::~Display() { }
+Display::~Display() { }
 
-    //-----------------------------------------------------------------
-    // Returning a supported video modes list
-    //
-    // TODO: Documentations
-    //
-    //-----------------------------------------------------------------
-    std::vector< VideoMode> Display::GetListVideoModes( ) const 
-    {
+//-----------------------------------------------------------------
+// Returning a supported video modes list
+//
+// TODO: Documentations
+//
+//-----------------------------------------------------------------
+std::vector< VideoMode> Display::GetListVideoModes() const 
+{
 
-        return cls::DisplayImpl::GetListVideoModes( name_.c_str( ) );
-    }   
+    return cls::DisplayImpl::GetListVideoModes(name_.c_str());
+}   
 
-    //---------------------------------------------------------------------
-    // Returning a current settings for instance
-    //
-    // TODO: Documentations
-    //
-    //---------------------------------------------------------------------
-    VideoMode Display::GetVideoMode( ) const 
-    {
+//---------------------------------------------------------------------
+// Returning a current settings for instance
+//
+// TODO: Documentations
+//
+//---------------------------------------------------------------------
+VideoMode Display::GetVideoMode() const 
+{
 
-        return cls::DisplayImpl::GetVideoMode( name_.c_str( ) );
-        
-    }
+    return cls::DisplayImpl::GetVideoMode(name_.c_str());
 
-    //-------------------------------------------------------------
-    // Setting up a custom mode for this display
-    //
-    // TODO: Documentations
-    //
-    //-------------------------------------------------------------
-    bool Display::SetVideoMode( const VideoMode& videoMode, uint32_t flags ) const
-    {
+}
 
-        if( !IsValidMode( videoMode ) ) return false;
-        
-        return cls::DisplayImpl::SetVideoMode( name_.c_str( ), videoMode, flags );
-    }
+//-------------------------------------------------------------
+// Setting up a custom mode for this display
+//
+// TODO: Documentations
+//
+//-------------------------------------------------------------
+bool Display::SetVideoMode(const VideoMode& videoMode, uint32_t flags) const
+{
 
-    //-------------------------------------------------------------
-    // Checking a selected mode
-    //
-    // TODO: Documentations
-    //
-    //-------------------------------------------------------------
-    bool Display::IsValidMode( const VideoMode& videoMode ) const 
-    {
+    if (!IsValidMode(videoMode)) return false;
 
-        return cls::DisplayImpl::SetVideoMode( name_.c_str(), 
-                                               videoMode, 
-                                               SML_VM_TEST );
-    }
+    return cls::DisplayImpl::SetVideoMode(name_.c_str(), videoMode, flags);
+}
 
-    // TODO: Description
-    // TODO: Documentation
-    bool Display::IsPrimary( ) const 
-    {
-        
-        return isPrimary_;
-    }
+//-------------------------------------------------------------
+// Checking a selected mode
+//
+// TODO: Documentations
+//
+//-------------------------------------------------------------
+bool Display::IsValidMode(const VideoMode& videoMode) const 
+{
 
-    //-------------------------------------------------------------------------
-    // Getting an adapters friendly description
-    //-------------------------------------------------------------------------   
-    std::string Display::GetAdapterDescription( ) const
-    {
-        return adapterDescription_;
-    }
+    return cls::DisplayImpl::SetVideoMode(name_.c_str(), videoMode, SML_VM_TEST);
+}
 
-    //-------------------------------------------------------------------------
-    // Getting an adapters friendly description
-    //-------------------------------------------------------------------------   
-    std::string Display::GetMonitorDescription( ) const
-    {
-        return monitorDescription_;
-    }
+// TODO: Description
+// TODO: Documentation
+bool Display::IsPrimary() const 
+{
 
-    bool operator ==( const Display& left, const Display& right ) 
-    {
-        auto _left = std::tie( left.name_,
-                               left.adapterDescription_,
-                               left.monitorDescription_,
-                               left.isPrimary_ );
+    return isPrimary_;
+}
 
-        auto _right = std::tie( right.name_,
-                                right.adapterDescription_,
-                                right.monitorDescription_,
-                                right.isPrimary_ );
+//-------------------------------------------------------------------------
+// Getting an adapters friendly description
+//-------------------------------------------------------------------------   
+std::string Display::GetAdapterDescription( ) const
+{
+    return adapterDescription_;
+}
 
-        return (_left == _right);
-    }
+//-------------------------------------------------------------------------
+// Getting an adapters friendly description
+//-------------------------------------------------------------------------   
+std::string Display::GetMonitorDescription( ) const
+{
+    return monitorDescription_;
+}
 
-    bool operator !=( const Display& left, const Display& right ) 
-    {
-        auto _left = std::tie( left.name_,
-                               left.adapterDescription_,
-                               left.monitorDescription_,
-                               left.isPrimary_ );
+bool operator ==( const Display& left, const Display& right ) 
+{
+    auto _left = std::tie( left.name_,
+                           left.adapterDescription_,
+                           left.monitorDescription_,
+                           left.isPrimary_ );
 
-        auto _right = std::tie( right.name_,
-                                right.adapterDescription_,
-                                right.monitorDescription_,
-                                right.isPrimary_ );
+    auto _right = std::tie( right.name_,
+                            right.adapterDescription_,
+                            right.monitorDescription_,
+                            right.isPrimary_ );
 
-        return (_left != _right);
-    }
+    return (_left == _right);
+}
+
+bool operator !=( const Display& left, const Display& right ) 
+{
+    auto _left = std::tie( left.name_,
+                           left.adapterDescription_,
+                           left.monitorDescription_,
+                           left.isPrimary_ );
+
+    auto _right = std::tie( right.name_,
+                            right.adapterDescription_,
+                            right.monitorDescription_,
+                            right.isPrimary_ );
+
+    return (_left != _right);
+}
 } // namespace video
 } // namespace sml
 //------------------------End of file Display.cpp -----------------------------
