3c9dfd6920b5ee2cc4f027cdf9f22a183af9b20a
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-28 00:22:22 +0700

    Update VideMode class
    A code style was changed.

--- include/SMLib/Video/VideoMode.h
+++ include/SMLib/Video/VideoMode.h
@@ -11,56 +11,64 @@ namespace sml {
 
 namespace video {
 
-struct SML_VIDEO_API VideoMode {
+class SML_VIDEO_API VideoMode
+{
 
-        //-----------------------------------------------------------------
-        // Default constructor
-        // Initialize member dields to default values
-        //
-        // m_width           : 640
-        // m_height          : 480
-        // m_bitPerPixel     : 32
-        // m_DisplayFrecancy : 60 Hz
-        //-----------------------------------------------------------------
-    VideoMode( );
+public:
+  //---------------------------------------------------------------------------
+  // Default constructor
+  // Initialize member dields to default values
+  //
+  // m_width           : 640
+  // m_height          : 480
+  // m_bitPerPixel     : 32
+  // m_DisplayFrecancy : 60 Hz
+  //---------------------------------------------------------------------------
+  VideoMode();
 
-        //-----------------------------------------------------------------
-        // Custom constructor with parameters
-        //-----------------------------------------------------------------
-    VideoMode( uint32_t width,
-                uint32_t height, 
-                uint32_t bitPerPixel = 32, 
-                uint32_t frecency    = 60 );
+  //---------------------------------------------------------------------------
+  // Custom constructor with parameters
+  //---------------------------------------------------------------------------
+  VideoMode(uint32_t width,
+            uint32_t height,
+            uint32_t bitPerPixel = 32,
+            uint32_t frecency = 60);
 
-        //-----------------------------------------------------------------
-        // Member data
-        //-----------------------------------------------------------------
-        uint32_t width;           
-        uint32_t height;          
-        uint32_t bitPerPixel;     
-        uint32_t displayFrecency;
+  //---------------------------------------------------------------------------
+  // Member data
+  //---------------------------------------------------------------------------
+  uint32_t width;
+  uint32_t height;
+  uint32_t bitPerPixel;
+  uint32_t displayFrecency;
 
 }; // struct VideoMode
 
 //-----------------------------------------------------------------------------
 // TODO Documentation
 //-----------------------------------------------------------------------------
-SML_VIDEO_API bool operator ==(const VideoMode& left, const VideoMode& right);
+SML_VIDEO_API bool
+operator==(const VideoMode& left, const VideoMode& right);
 
 //-----------------------------------------------------------------------------
 // TODO Documentation
 //-----------------------------------------------------------------------------
-SML_VIDEO_API bool operator !=(const VideoMode& left, const VideoMode& right);
+SML_VIDEO_API bool
+operator!=(const VideoMode& left, const VideoMode& right);
 
 //-----------------------------------------------------------------------------
-// Disable 
+// Disable
 //
 // TODO Documtation
 //-----------------------------------------------------------------------------
-bool operator >(const VideoMode& left, const VideoMode& right)  = delete;
-bool operator <(const VideoMode& left, const VideoMode& right)  = delete;
-bool operator >=(const VideoMode& left, const VideoMode& right) = delete;
-bool operator <=(const VideoMode& left, const VideoMode& right) = delete;
+bool
+operator>(const VideoMode& left, const VideoMode& right) = delete;
+bool
+operator<(const VideoMode& left, const VideoMode& right) = delete;
+bool
+operator>=(const VideoMode& left, const VideoMode& right) = delete;
+bool
+operator<=(const VideoMode& left, const VideoMode& right) = delete;
 
 } // namespace video
 } // namspace sml
--- src/SMLib/Video/VideoMode.cpp
+++ src/SMLib/Video/VideoMode.cpp
@@ -3,8 +3,8 @@
 //-----------------------------------------------------------------------------
 // Headers
 //-----------------------------------------------------------------------------
-#include <tuple>
 #include <SMLib/Video/VideoMode.h>
+#include <tuple>
 
 namespace sml {
 namespace video {
@@ -12,50 +12,48 @@ namespace video {
 //---------------------------------------------------------------------
 // Default constructor
 //---------------------------------------------------------------------
-VideoMode::VideoMode() 
-: width           (640),
-  height          (480), 
-  bitPerPixel     (32),
-  displayFrecency (60) { }
+VideoMode::VideoMode()
+  : width(640)
+  , height(480)
+  , bitPerPixel(32)
+  , displayFrecency(60)
+{}
 
 //---------------------------------------------------------------------
 // Custom constructor
 //---------------------------------------------------------------------
-VideoMode::VideoMode(uint32_t width, uint32_t height, 
-                     uint32_t bitPerPixel, uint32_t frecancy) 
-: width           ( width ), 
-  height          ( height ), 
-  bitPerPixel     ( bitPerPixel ),
-  displayFrecency ( frecancy ) { }
+VideoMode::VideoMode(uint32_t width,
+                     uint32_t height,
+                     uint32_t bitPerPixel,
+                     uint32_t frecancy)
+  : width(width)
+  , height(height)
+  , bitPerPixel(bitPerPixel)
+  , displayFrecency(frecancy)
+{}
 
-bool operator ==(const VideoMode& left, const VideoMode& right)
+bool
+operator==(const VideoMode& left, const VideoMode& right)
 {
-     auto _left = std::tie( left.width, 
-                            left.height, 
-                            left.bitPerPixel,
-                            left.displayFrecency );
+  auto _left =
+    std::tie(left.width, left.height, left.bitPerPixel, left.displayFrecency);
 
-    auto  _right = std::tie( right.width,
-                             right.height,
-                             right.bitPerPixel,
-                             right.displayFrecency );
+  auto _right = 
+    std::tie(right.width, right.height, right.bitPerPixel, right.displayFrecency);
 
-    return (_left == _right);
+  return (_left == _right);
 }
 
-bool operator !=(const VideoMode& left, const VideoMode& right)
+bool
+operator!=(const VideoMode& left, const VideoMode& right)
 {
-    auto _left = std::tie( left.width, 
-                           left.height, 
-                           left.bitPerPixel,
-                           left.displayFrecency );
+  auto _left =
+    std::tie(left.width, left.height, left.bitPerPixel, left.displayFrecency);
 
-    auto  _right = std::tie( right.width,
-                             right.height,
-                             right.bitPerPixel,
-                             right.displayFrecency );
+  auto _right = 
+    std::tie(right.width, right.height, right.bitPerPixel, right.displayFrecency);
 
-    return (_left != _right);
+  return (_left != _right);
 }
 
 } // namespace video
