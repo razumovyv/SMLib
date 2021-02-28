8edefa8e9fbb09948fcafcd5e573b484b80c97a1
Yuriy Razumov <razumov.uv@gmail.com>
2021-02-28 19:55:15 +0700

    Changed testApp
    Testing new realization.

--- tests/test-app/testApp.cpp
+++ tests/test-app/testApp.cpp
@@ -5,10 +5,13 @@ using namespace sml::video;
 
 int main(int argc, char* argv[])
 {
+  const DisplayContainer& displays = DisplayContainer::Create();
+  PDisplay display = displays[0];
+  const PDisplay& display_2 = displays[0];
+  VideoMode mode = display_2->GetVideoMode();
 
-	auto displayList = Display::GetDisplaysList();
-	const DisplayPtr display = displayList[0];
-	VideoMode mode = display->GetVideoMode();
-    std::cout << "width: " << mode.width << std::endl;
-	return 0;
+
+  std::cout << mode.width << "x" << mode.height << std::endl;
+  std::cin.get();
+  return 0;
 }
